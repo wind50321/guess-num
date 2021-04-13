34598141df1ff974839733060aeb7533d1096bec
Jason Chen <wind50321@gmail.com>
2021-04-13 22:58:35 +0800

    add count

--- guess.py
+++ guess.py
@@ -1,9 +1,12 @@
 import random
 
 answer = random.randint(1, 100) #產生隨機整數，包含1和100
+count = 0
 while True:
+    count += 1 # count = count + 1
     num = input('請猜數字： ')
     num = int(num)
+    print('猜第', count, '次')
     if num == answer:
         print('你猜對了！')
         break
